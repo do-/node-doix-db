0a7e5ed8247cbf695e9b55533fc73d99610a7181
Dmitry Ovsyanko <do_@pochta.ru>
2023-01-29 17:30:55 +0300

    DbQueryTable: join, on; DbQuery (from)

--- __tests__/DbQuery.js
+++ __tests__/DbQuery.js
@@ -20,6 +20,68 @@ test ('bad', () => {
 	const q = new DbQuery (m)
 	expect (() => new DbQueryTable (0, 'userz')).toThrow ()
 
+	expect (() => {
+		m.createQuery ([
+			['users', {
+				join: 'CROSS'
+			}],
+			['roles', {
+				as: 'r', 
+				columns: ['label']
+			}],
+		])
+	}).toThrow ()
+
+	expect (() => {
+		m.createQuery ([
+			['users', {
+			}],
+			['roles', {
+				join: 'RIGHT',
+				as: 'r', 
+				columns: ['label']
+			}],
+		])
+	}).toThrow ()
+
+	expect (() => {
+		m.createQuery ([
+			['users', {
+			}],
+			['roles', {
+				join: 'CROSS',
+				on: 'users.id_role',
+				as: 'r', 
+				columns: ['label']
+			}],
+		])
+	}).toThrow ()
+
+	expect (() => {
+		m.createQuery ([
+			['users', {
+				as: 'u1',
+			}],
+			['users', {
+				as: 'u2',
+			}],
+		])
+	}).toThrow ()
+
+	expect (() => {
+		m.createQuery ([
+			['users', {
+				as: 'u1',
+			}],
+			['users', {
+				as: 'u2',
+				on: 'u1.uuid=u2.uuid'
+			}],
+			['roles'],
+		])
+	}).toThrow ()
+
+
 })
 
 test ('not in model', () => {
@@ -28,9 +90,14 @@ test ('not in model', () => {
 	const m = new DbModel ({dir, foo: undefined})	
 	m.loadModules ()
 
-	const q = m.createQuery ()
-	const dual = new DbQueryTable (q, 'DUAL')
-	
+	const q = m.createQuery ([
+		['users'],
+		['DUAL'],
+		['DUAL', {as: '2'}],
+		['DUAL', {as: '3', on: '2.dummy = 3.dummy'}],
+		['roles'],
+	])
+
 })
 
 test ('basic', () => {
@@ -39,12 +106,56 @@ test ('basic', () => {
 	const m = new DbModel ({dir, foo: undefined})	
 	m.loadModules ()
 
-	const q = m.createQuery ()
-	const u = q.addTable ('users')
+	const q = m.createQuery ([
+		['users'],
+		['roles', {
+			as: 'r', 
+			columns: ['label']
+		}],
+	])
 
-	expect (q.toParamsSql ()).toStrictEqual (['SELECT "users"."uuid" AS "uuid","users"."label" AS "label","users"."id_role" AS "id_role" FROM "users" AS "users"'])
+	expect (q.toParamsSql ()).toStrictEqual (['SELECT "users"."uuid" AS "uuid","users"."label" AS "label","users"."id_role" AS "id_role","r"."label" AS "r.label" FROM "users" AS "users" LEFT JOIN "roles" AS "r" ON "r"."id"="users"."id_role"'])
 						
 })
+
+test ('join by ref', () => {
+
+	jest.resetModules ()
+	const m = new DbModel ({dir, foo: undefined})	
+	m.loadModules ()
+
+	const q = m.createQuery ([
+		['users'],
+		['roles', {
+			as: 'r', 
+			on: 'users.id_role', 
+			columns: ['label']
+		}],
+	])
+
+	expect (q.toParamsSql ()).toStrictEqual (['SELECT "users"."uuid" AS "uuid","users"."label" AS "label","users"."id_role" AS "id_role","r"."label" AS "r.label" FROM "users" AS "users" LEFT JOIN "roles" AS "r" ON "r"."id"="users"."id_role"'])
+						
+})
+
+test ('explicit join', () => {
+
+	jest.resetModules ()
+	const m = new DbModel ({dir, foo: undefined})	
+	m.loadModules ()
+
+	const q = m.createQuery ([
+		['users'],
+		['roles', {
+			as: 'r', 
+			on: 'users.id_role = r.id', 
+			columns: ['label']
+		}],
+	])
+
+	expect (q.toParamsSql ()).toStrictEqual (['SELECT "users"."uuid" AS "uuid","users"."label" AS "label","users"."id_role" AS "id_role","r"."label" AS "r.label" FROM "users" AS "users" LEFT JOIN "roles" AS "r" ON users.id_role = r.id'])
+						
+})
+
 
 test ('ord', () => {
 
@@ -53,7 +164,7 @@ test ('ord', () => {
 	m.loadModules ()
 
 	const q = m.createQuery ()
-	const u = q.addTable ('users', {alias: 'userz', columns: ['id_role', 'label']})
+	const u = q.addTable ('users', {as: 'userz', columns: ['id_role', 'label']})
 
 	q.orderBy ('id_role', true)
 	q.orderBy ('label')
