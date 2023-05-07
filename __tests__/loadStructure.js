ffdf4d40c5c0e5f0487306cb7d9a6b913abc4d35
Dmitry Ovsyanko <do_@pochta.ru>
2023-05-07 15:03:39 +0300

    DbMigrationPlan loadStructure

--- __tests__/loadStructure.js
+++ __tests__/loadStructure.js
@@ -3,14 +3,14 @@ const MockDb = require ('./lib/MockDb.js')
 test ('createMigrationPlan', async () => {
 		
 	const db = new MockDb ()
-	
+
 	const unknown = []
 
 	const plan = db.createMigrationPlan ()
 	
 	plan.on ('unknown', t => unknown.push (t))
 	
-	await plan.loadExistingTables ()
+	await plan.loadStructure ()
 	
 	expect (unknown.map (i => i.name)).toStrictEqual (['__alien'])
 	expect ([...plan.asIs.keys ()]).toStrictEqual (['users'])
