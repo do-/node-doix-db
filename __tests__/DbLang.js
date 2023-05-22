d16151176799b53850ce9b236afe138a142810e1
Dmitry Ovsyanko <do_@pochta.ru>
2023-05-22 20:07:31 +0300

    +genColumn

--- __tests__/DbLang.js
+++ __tests__/DbLang.js
@@ -296,6 +296,24 @@ test ('genColumnDefault', () => {
 
 })
 
+test ('genColumn', () => {
+	
+	const column = new DbColumn ('int=1')
+	column.name = 'id'
+	column.setLang (lang)
+
+	expect (lang.genColumn (column)).toBe (`"id" INT DEFAULT '1' NOT NULL`)
+
+	column.nullable = true
+
+	expect (lang.genColumn (column)).toBe (`"id" INT DEFAULT '1'`)
+
+	delete column.default
+
+	expect (lang.genColumn (column)).toBe (`"id" INT`)
+
+})
+
 test ('compareColumns', () => {
 
 	expect (	
