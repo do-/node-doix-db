b953125c87e51b40ff24ef004946a9fc307d014a
Dmitry Ovsyanko <do_@pochta.ru>
2023-10-02 12:20:52 +0300

    +DbSchemaSource.js

---
+++ __tests__/DbSchemaSource.js
@@ -0,0 +1,26 @@
+const {DbSchemaSource} = require ('..')
+
+test ('bad', () => {
+
+	expect (() => new DbSchemaSource ({})).toThrow ()
+	expect (() => new DbSchemaSource ({dir: 1, root: 2})).toThrow ()
+
+})
+
+test ('scalar root', () => {
+
+	const s = new DbSchemaSource ({root: '/', filter: () => true})
+
+	expect (s.loader.dir.dir.root).toStrictEqual (['/'])
+
+	expect (s.loader.dir.dir.filter ()).toBe (true)
+
+})
+
+test ('array root', () => {
+
+	const root = ['/1', '/2'], s = new DbSchemaSource ({root})
+
+	expect (s.loader.dir.dir.root).toStrictEqual (root)
+
+})
