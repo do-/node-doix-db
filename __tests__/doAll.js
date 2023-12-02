c36136be7d629332e93b18dfb35f6854c67e2a01
Dmitry Ovsyanko <do_@pochta.ru>
2023-12-02 18:02:40 +0300

    doAll test

---
+++ __tests__/doAll.js
@@ -0,0 +1,53 @@
+const MockDb = require ('./lib/MockDb.js')
+const {Readable, Writable} = require ('stream')
+
+test ('misc', async () => {
+
+	const db = new MockDb ()
+
+	expect (db.batch ()).toBeInstanceOf (Writable)
+
+	await expect (db.doAll (1)).rejects.toThrow ()
+	await expect (db.doAll (null)).rejects.toThrow ()
+	await expect (db.doAll ({})).rejects.toThrow ()
+
+})
+
+
+test ('doAll array', async () => {
+
+	const db = new MockDb (), {trackerClass} = db.pool
+
+	const a = []; db.pool = {trackerClass, logger: {log: m => a.push (m.message)}}
+
+	await db.doAll ([
+		{sql: 'COMMIT'},
+		{sql: 'COMMIT'},
+	])
+
+	expect (a).toHaveLength (4)
+	expect (a [0]).toMatch (/ > /)
+	expect (a [1]).toMatch (/ < /)
+	expect (a [2]).toMatch (/ > /)
+	expect (a [3]).toMatch (/ < /)
+	
+})
+
+test ('doAll stream', async () => {
+
+	const db = new MockDb (), {trackerClass} = db.pool
+
+	const a = []; db.pool = {trackerClass, logger: {log: m => a.push (m.message)}}
+
+	await db.doAll (Readable.from ([
+		{sql: 'COMMIT'},
+		{sql: 'COMMIT'},
+	]))
+
+	expect (a).toHaveLength (4)
+	expect (a [0]).toMatch (/ > /)
+	expect (a [1]).toMatch (/ < /)
+	expect (a [2]).toMatch (/ > /)
+	expect (a [3]).toMatch (/ < /)
+	
+})
